4c7d65c931a811d2b8a493e1d951102e366256bb
John Arne Føreland <john.foreland@me.com>
2025-06-17 13:08:35 +0200

    fixed lint errors on tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -7,13 +7,13 @@ jest.mock('../src/action-inputs')
 describe('EMPTY TEXT', () => {
   it('no change (empty string)', async () => {
     const mock: Partial<IActionInputs> = { ...getMock(0), MAX_LINES: 0 }
-    ;(getInputs as any).mockImplementation(() => mock)
+    ;(getInputs as jest.Mock).mockImplementation(() => mock)
     const result = new TruncateTool().truncate()
     expect(result).toBe('')
   })
   it('no truncate (empty string)', async () => {
     const mock: Partial<IActionInputs> = { ...getMock(0), MAX_LINES: 5 }
-    ;(getInputs as any).mockImplementation(() => mock)
+    ;(getInputs as jest.Mock).mockImplementation(() => mock)
     const result = new TruncateTool().truncate()
     expect(result).toBe('')
   })
@@ -23,13 +23,13 @@ describe('1 line in input', () => {
   describe('MAX_LINES', () => {
     it('truncates to 0 lines', async () => {
       const mock: Partial<IActionInputs> = { ...getMock(1), MAX_LINES: 0 }
-      ;(getInputs as any).mockImplementation(() => mock)
+      ;(getInputs as jest.Mock).mockImplementation(() => mock)
       const result = new TruncateTool().truncate()
       expect(result).toBe('')
     })
     it('truncates to max 5 lines', async () => {
       const mock: Partial<IActionInputs> = { ...getMock(1), MAX_LINES: 5 }
-      ;(getInputs as any).mockImplementation(() => mock)
+      ;(getInputs as jest.Mock).mockImplementation(() => mock)
       const result = new TruncateTool().truncate()
       expect(result).toBe('Lorem ipsum dolor sit amet')
     })
@@ -38,19 +38,19 @@ describe('1 line in input', () => {
   describe('MAX_CHARACTERS', () => {
     it('truncates to max 0 characters', async () => {
       const mock: Partial<IActionInputs> = { ...getMock(1), MAX_CHARACTERS: 0 }
-      ;(getInputs as any).mockImplementation(() => mock)
+      ;(getInputs as jest.Mock).mockImplementation(() => mock)
       const result = new TruncateTool().truncate()
       expect(result).toBe('')
     })
     it('truncates to max 3 characters', async () => {
       const mock: Partial<IActionInputs> = { ...getMock(1), MAX_CHARACTERS: 3 }
-      ;(getInputs as any).mockImplementation(() => mock)
+      ;(getInputs as jest.Mock).mockImplementation(() => mock)
       const result = new TruncateTool().truncate()
       expect(result).toBe('Lor')
     })
     it('truncates to max 9 characters', async () => {
       const mock: Partial<IActionInputs> = { ...getMock(1), MAX_CHARACTERS: 9 }
-      ;(getInputs as any).mockImplementation(() => mock)
+      ;(getInputs as jest.Mock).mockImplementation(() => mock)
       const result = new TruncateTool().truncate()
       expect(result).toBe('Lorem ips')
     })
@@ -61,13 +61,13 @@ describe('10 lines in input', () => {
   describe('MAX_LINES', () => {
     it('truncates to 0 lines', async () => {
       const mock: Partial<IActionInputs> = { ...getMock(10), MAX_LINES: 0 }
-      ;(getInputs as any).mockImplementation(() => mock)
+      ;(getInputs as jest.Mock).mockImplementation(() => mock)
       const result = new TruncateTool().truncate()
       expect(result).toBe('')
     })
     it('truncates to max 5 lines', async () => {
       const mock: Partial<IActionInputs> = { ...getMock(10), MAX_LINES: 5 }
-      ;(getInputs as any).mockImplementation(() => mock)
+      ;(getInputs as jest.Mock).mockImplementation(() => mock)
       const result = new TruncateTool().truncate()
       expect(result).toBe(
         'Lorem ipsum dolor sit amet\nLorem ipsum dolor sit amet\nLorem ipsum dolor sit amet\nLorem ipsum dolor sit amet\nLorem ipsum dolor sit amet'
@@ -78,25 +78,25 @@ describe('10 lines in input', () => {
   describe('MAX_CHARACTERS', () => {
     it('truncates to max 0 characters', async () => {
       const mock: Partial<IActionInputs> = { ...getMock(10), MAX_CHARACTERS: 0 }
-      ;(getInputs as any).mockImplementation(() => mock)
+      ;(getInputs as jest.Mock).mockImplementation(() => mock)
       const result = new TruncateTool().truncate()
       expect(result).toBe('')
     })
     it('truncates to max 3 characters', async () => {
       const mock: Partial<IActionInputs> = { ...getMock(10), MAX_CHARACTERS: 3 }
-      ;(getInputs as any).mockImplementation(() => mock)
+      ;(getInputs as jest.Mock).mockImplementation(() => mock)
       const result = new TruncateTool().truncate()
       expect(result).toBe('Lor')
     })
     it('truncates to max 9 characters', async () => {
       const mock: Partial<IActionInputs> = { ...getMock(10), MAX_CHARACTERS: 9 }
-      ;(getInputs as any).mockImplementation(() => mock)
+      ;(getInputs as jest.Mock).mockImplementation(() => mock)
       const result = new TruncateTool().truncate()
       expect(result).toBe('Lorem ips')
     })
     it('truncates to max 30 characters', async () => {
       const mock: Partial<IActionInputs> = { ...getMock(10), MAX_CHARACTERS: 30 }
-      ;(getInputs as any).mockImplementation(() => mock)
+      ;(getInputs as jest.Mock).mockImplementation(() => mock)
       const result = new TruncateTool().truncate()
       expect(result).toBe('Lorem ipsum dolor sit amet\nLor')
     })
@@ -106,43 +106,43 @@ describe('STRING_TO_APPEND', () => {
   describe('MAX_LINES', () => {
     it('1 line truncated to 0 lines', async () => {
       const mock: Partial<IActionInputs> = { ...getMock(1), MAX_LINES: 0, STRING_TO_APPEND: '...' }
-      ;(getInputs as any).mockImplementation(() => mock)
+      ;(getInputs as jest.Mock).mockImplementation(() => mock)
       const result = new TruncateTool().truncate()
       expect(result).toBe('')
     })
     it('1 line truncated to max 5 lines', async () => {
       const mock: Partial<IActionInputs> = { ...getMock(1), MAX_LINES: 5, STRING_TO_APPEND: '...' }
-      ;(getInputs as any).mockImplementation(() => mock)
+      ;(getInputs as jest.Mock).mockImplementation(() => mock)
       const result = new TruncateTool().truncate()
       expect(result).toBe('Lorem ipsum dolor sit amet')
     })
     it('2 lines truncated to max 1 lines', async () => {
       const mock: Partial<IActionInputs> = { ...getMock(2), MAX_LINES: 1, STRING_TO_APPEND: '...' }
-      ;(getInputs as any).mockImplementation(() => mock)
+      ;(getInputs as jest.Mock).mockImplementation(() => mock)
       const result = new TruncateTool().truncate()
       expect(result).toBe('Lorem ipsum dolor sit amet\n\n...')
     })
     it('2 lines truncated to max 2 lines', async () => {
       const mock: Partial<IActionInputs> = { ...getMock(2), MAX_LINES: 2, STRING_TO_APPEND: '...' }
-      ;(getInputs as any).mockImplementation(() => mock)
+      ;(getInputs as jest.Mock).mockImplementation(() => mock)
       const result = new TruncateTool().truncate()
       expect(result).toBe('Lorem ipsum dolor sit amet\nLorem ipsum dolor sit amet')
     })
     it('2 lines truncated to max 3 lines', async () => {
       const mock: Partial<IActionInputs> = { ...getMock(2), MAX_LINES: 3, STRING_TO_APPEND: '...' }
-      ;(getInputs as any).mockImplementation(() => mock)
+      ;(getInputs as jest.Mock).mockImplementation(() => mock)
       const result = new TruncateTool().truncate()
       expect(result).toBe('Lorem ipsum dolor sit amet\nLorem ipsum dolor sit amet')
     })
     it('10 lines truncated to 0 lines', async () => {
       const mock: Partial<IActionInputs> = { ...getMock(10), MAX_LINES: 0, STRING_TO_APPEND: '...' }
-      ;(getInputs as any).mockImplementation(() => mock)
+      ;(getInputs as jest.Mock).mockImplementation(() => mock)
       const result = new TruncateTool().truncate()
       expect(result).toBe('')
     })
     it('10 lines truncated to max 5 lines', async () => {
       const mock: Partial<IActionInputs> = { ...getMock(10), MAX_LINES: 5, STRING_TO_APPEND: '...' }
-      ;(getInputs as any).mockImplementation(() => mock)
+      ;(getInputs as jest.Mock).mockImplementation(() => mock)
       const result = new TruncateTool().truncate()
       expect(result).toBe('Lorem ipsum dolor sit amet\nLorem ipsum dolor sit amet\nLorem ipsum dolor sit amet\n\n...')
     })
@@ -151,19 +151,19 @@ describe('STRING_TO_APPEND', () => {
   describe('MAX_CHARACTERS', () => {
     it('26 characters truncated to max 0 characters', async () => {
       const mock: Partial<IActionInputs> = { ...getMock(1), MAX_CHARACTERS: 0, STRING_TO_APPEND: '...' }
-      ;(getInputs as any).mockImplementation(() => mock)
+      ;(getInputs as jest.Mock).mockImplementation(() => mock)
       const result = new TruncateTool().truncate()
       expect(result).toBe('')
     })
     it('26 characters truncated to max 3 characters', async () => {
       const mock: Partial<IActionInputs> = { ...getMock(1), MAX_CHARACTERS: 3, STRING_TO_APPEND: '...' }
-      ;(getInputs as any).mockImplementation(() => mock)
+      ;(getInputs as jest.Mock).mockImplementation(() => mock)
       const result = new TruncateTool().truncate()
       expect(result).toBe('Lor')
     })
     it('26 characters truncated to max 9 characters', async () => {
       const mock: Partial<IActionInputs> = { ...getMock(1), MAX_CHARACTERS: 9, STRING_TO_APPEND: '...' }
-      ;(getInputs as any).mockImplementation(() => mock)
+      ;(getInputs as jest.Mock).mockImplementation(() => mock)
       const result = new TruncateTool().truncate()
       expect(result).toBe('Lorem ...')
     })
